e47453ab60d0c028994da900b6e3ccad4b98bebd
Yuan Zhang <z5122988@ad.unsw.edu.au>
2021-10-11 11:35:07 +0800

    laser process done. to complete the pm part of laser

--- Laser/Laser.cpp
+++ Laser/Laser.cpp
@@ -8,8 +8,8 @@ int Laser::connect(String^ hostName, int portNumber)
 	Client->NoDelay = TRUE;
 	Client->ReceiveTimeout = 2000;
 	Client->SendTimeout = 1000;
-	Client->ReceiveBufferSize = 1024;
-	Client->SendBufferSize = 1024;
+	Client->ReceiveBufferSize = 2048;
+	Client->SendBufferSize = 32;
 
 	Stream = Client->GetStream();
 
@@ -23,30 +23,42 @@ int Laser::Auth(String^ zid) {  //gcnew string
 	array<unsigned char>^ IDAuthentication;
 	IDAuthentication = gcnew array<unsigned char>(512);
 	Stream->Read(IDAuthentication, 0, IDAuthentication->Length);
+	System::String^ ResponseData;
+	ResponseData = System::Text::Encoding::ASCII->GetString(IDAuthentication);
+	if (ResponseData->Contains ("OK")) {
+		return 1;
+	}
+	else {
+		Console::WriteLine(ResponseData);
+		return 0;
+	}
+	
 
-	return 1;
 };
 void Laser::printData() {
-	Console::WriteLine("print data");
+	for (int i = 0; i < NumRanges; i++) {
+
+		Console::WriteLine("Laser[{0}] Angle->{1} X->{2} Y->{3} Radius->{4}", i, StartAngle + Resolution * i, RangeX[i], RangeY[i],Range[i]);
+	}
 };
 int Laser::setupSharedMemory()
 {
 	Console::WriteLine("Setting up shared memory");
 //	Console::WriteLine("Setting up shared memory");
-//	ProcessManagementData = new SMObject(_TEXT("PM_SM"), sizeof(ProcessManagement));
-//	SensorData = new SMObject(_TEXT("Laser_SM"), sizeof(SM_Laser));
-//	ProcessManagementData->SMAccess();
-//	SensorData->SMAccess();
+	ProcessManagementData = new SMObject(_TEXT("PM_SM"), sizeof(ProcessManagement));
+	SensorData = new SMObject(_TEXT("Laser_SM"), sizeof(SM_Laser));
+	ProcessManagementData->SMAccess();
+	SensorData->SMAccess();
 //
-//	if (SensorData->SMAccessError)
-//	{
-//		Console::WriteLine("Share memory access failed");
-//		return -2;
-//	}
+	if (SensorData->SMAccessError)
+	{
+		Console::WriteLine("Share memory access failed");
+		return -2;
+	}
 //
-//	PMdata = (ProcessManagement*)ProcessManagementData->pData;
-//	Laser_info = (SM_Laser*)SensorData->pData;
-//	Console::WriteLine("Setting up shared memory finished");
+	PMdata = (ProcessManagement*)ProcessManagementData->pData;
+	Laser_info = (SM_Laser*)SensorData->pData;
+	Console::WriteLine("Setting up shared memory finished");
 	return 1;
 };
 int Laser::getData()
@@ -62,6 +74,7 @@ int Laser::getData()
 	Stream->Write(SendData, 0, SendData->Length);
 	Stream->WriteByte(0x03);
 	Stream->Read(ReadData, 0, ReadData->Length);
+
 	return 1;
 };
 int Laser::processData()
@@ -69,6 +82,7 @@ int Laser::processData()
 	String^ LaserScan = nullptr;
 	LaserScan = System::Text::Encoding::ASCII->GetString(ReadData);
 	Fragments = LaserScan->Split(' ');
+
 
 	StartAngle = System::Convert::ToInt32(Fragments[23], 16);
 	Resolution = System::Convert::ToInt32(Fragments[24], 16) / 10000.0;
@@ -83,28 +97,36 @@ int Laser::processData()
 		Range[i] = System::Convert::ToInt32(Fragments[26 + i], 16);
 		RangeX[i] = Range[i] * Math::Sin(i * Resolution * Math::PI / 180.0);
 		RangeY[i] = Range[i] * Math::Cos(i * Resolution * Math::PI / 180.0);
-
-
-
-
-		if (i < 5) { //debug purpose
-			Console::WriteLine("{0,10:F3} {1,10:F3} {2,10:F3}", Range[i], RangeX[i], RangeY[i]);
-		}
 	}
 	return 1;
 }
 
 int Laser::checkData() {
-	Console::WriteLine("Setting up shared memory");
+
+	if (NumRanges != 361) {
+		return 0;
+	};
+
+	if (Fragments[1] != "LMDscandata") {
+		return 0;
+	}
 	return 1;
 };
 int Laser::sendDataToSharedMemory() {
+	for (int i = 0; i < 361; i++) {
+		Laser_info->x[i] = RangeX[i];
+		Laser_info->y[i] = RangeY[i];
+	}
+
 	return 1;
 };
 bool Laser::getShutdownFlag() {
-	return FALSE;
+	bool shutdown = PMdata->Shutdown.Flags.GPS;
+	return shutdown;
 };
 int Laser::setHeartbeat(bool heartbeat) {
+	PMdata->Heartbeat.Flags.GPS = heartbeat;
+	// YOUR CODE HERE
 	return 1;
 };
 Laser::~Laser() {
--- Laser/LaserMain.cpp
+++ Laser/LaserMain.cpp
@@ -1,5 +1,18 @@
 #include "Laser.h"
 int main() {
 	Laser^ lasermodule = gcnew Laser();
-	lasermodule->connect("192.168.1.200", 25000);
+	lasermodule->connect("192.168.1.200", 23000);
+
+	String^ zid = nullptr;
+	zid = gcnew String("5122988\n");
+	lasermodule->Auth(zid);
+
+
+	lasermodule->getData();
+	lasermodule->processData();
+	lasermodule->checkData();
+	//lasermodule->processData();
+	//lasermodule->printData();
+
+	Console::ReadKey();
 }
